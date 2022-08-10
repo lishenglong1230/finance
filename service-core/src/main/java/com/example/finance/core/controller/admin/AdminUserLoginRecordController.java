eb2ad42f8a7fb232937c9dc931d67ba08269e974
“lishenglong1230” <1215288388@qq.com>
2022-08-10 17:03:25 +0800

    后台管理系统 - 获取会员登录日志

---
+++ service-core/src/main/java/com/example/finance/core/controller/admin/AdminUserLoginRecordController.java
@@ -0,0 +1,38 @@
+package com.example.finance.core.controller.admin;
+
+import com.example.common.result.R;
+import com.example.finance.core.mapper.UserLoginRecordMapper;
+import com.example.finance.core.pojo.entity.UserLoginRecord;
+import com.example.finance.core.service.UserLoginRecordService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author: Lishenglong
+ * @Date: 2022/8/10 11:17
+ */
+@Api(tags = "会员登录日志接口")
+@RestController
+@RequestMapping("/admin/core/userLoginRecord")
+@Slf4j
+@CrossOrigin
+public class AdminUserLoginRecordController {
+    @Resource
+    UserLoginRecordService userLoginRecordService;
+
+    @ApiOperation("获取会员登录日志列表")
+    @GetMapping("/listTop50/{userId}")
+    public R listTop50(
+            @ApiParam(value = "用户id", required = true)
+            @PathVariable Long userId){
+
+        List<UserLoginRecord> userLoginRecordList = userLoginRecordService.listTop50(userId);
+        return R.ok().data("list",userLoginRecordList);
+    }
+}
