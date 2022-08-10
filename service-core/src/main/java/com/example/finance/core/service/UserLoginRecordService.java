7294ae3862246a92d385e131142333a52a838f8d
“lishenglong1230” <1215288388@qq.com>
2022-08-10 13:50:12 +0800

    后台管理系统 - 会员登录日志表

--- service-core/src/main/java/com/example/finance/core/service/UserLoginRecordService.java
+++ service-core/src/main/java/com/example/finance/core/service/UserLoginRecordService.java
@@ -3,6 +3,8 @@ package com.example.finance.core.service;
 import com.example.finance.core.pojo.entity.UserLoginRecord;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  * 用户登录记录表 服务类
@@ -13,4 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UserLoginRecordService extends IService<UserLoginRecord> {
 
+    List<UserLoginRecord> listTop50(Long userId);
 }
--- service-core/src/main/java/com/example/finance/core/service/impl/UserLoginRecordServiceImpl.java
+++ service-core/src/main/java/com/example/finance/core/service/impl/UserLoginRecordServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.finance.core.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.finance.core.pojo.entity.UserLoginRecord;
 import com.example.finance.core.mapper.UserLoginRecordMapper;
 import com.example.finance.core.service.UserLoginRecordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserLoginRecordServiceImpl extends ServiceImpl<UserLoginRecordMapper, UserLoginRecord> implements UserLoginRecordService {
 
+    @Override
+    public List<UserLoginRecord> listTop50(Long userId) {
+
+        QueryWrapper<UserLoginRecord> userLoginRecordQueryWrapper = new QueryWrapper<>();
+        userLoginRecordQueryWrapper.eq("user_id",userId)
+                                    .orderByDesc("id")
+                                    .last("limit 50");
+        List<UserLoginRecord> userLoginRecordList = baseMapper.selectList(userLoginRecordQueryWrapper);
+        return userLoginRecordList;
+
+    }
 }
