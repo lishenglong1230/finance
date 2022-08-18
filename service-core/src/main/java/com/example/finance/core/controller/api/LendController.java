b11dc641a0228e7aa7eae10938a437490b09dbe2
“lishenglong1230” <1215288388@qq.com>
2022-08-18 14:42:42 +0800

    获取标列表

---
+++ service-core/src/main/java/com/example/finance/core/controller/api/LendController.java
@@ -0,0 +1,42 @@
+package com.example.finance.core.controller.api;
+
+
+import com.example.common.result.R;
+import com.example.finance.core.pojo.entity.Lend;
+import com.example.finance.core.service.LendService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 标的准备表 前端控制器
+ * </p>
+ *
+ * @author lishenglong
+ * @since 2022-07-29
+ */
+@Api(tags = "标的")
+@RestController
+@RequestMapping("/api/core/lend")
+@Slf4j
+public class LendController {
+
+    @Resource
+    private LendService lendService;
+
+    @ApiOperation("获取标的列表")
+    @GetMapping("/list")
+    public R list() {
+        List<Lend> lendList = lendService.selectList();
+        return R.ok().data("lendList", lendList);
+    }
+}
+
